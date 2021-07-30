eae8a3e1332a1f0f9ae64b260eda5f936d235342
Tazo Leladze <t.leladze@omedia.ge>
2021-07-30 19:58:38 +0400

    code formating

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,54 +1,57 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback } from "react";
 
-const useModal = <T extends any>(initialState = false, initState = {}): IReturn<T> => {
-	const [isOpen, setOpen] = useState(initialState);
-	const [showSuccess, setSuccess] = useState(false);
-	const [showProgress, setProgress] = useState(false);
-	const [showError, setError] = useState(false);
-	const [item, setSelectedItem] = useState(initState);
+const useModal = <T extends any>(
+  initialState = false,
+  initState = {}
+): IReturn<T> => {
+  const [isOpen, setOpen] = useState(initialState);
+  const [showSuccess, setSuccess] = useState(false);
+  const [showProgress, setProgress] = useState(false);
+  const [showError, setError] = useState(false);
+  const [item, setSelectedItem] = useState(initState);
 
-	const closeModal = useCallback(() => {
-		setSelectedItem({});
-		setOpen(false);
-	}, [setOpen]);
+  const closeModal = useCallback(() => {
+    setSelectedItem({});
+    setOpen(false);
+  }, [setOpen]);
 
-	const openModal = useCallback(
-		(setItem = null) => {
-			setSelectedItem(setItem);
-			setSuccess(false);
-			setProgress(false);
-			setError(false);
-			setOpen(true);
-		},
-		[setSuccess, setProgress, setError, setOpen]
-	);
+  const openModal = useCallback(
+    (setItem = null) => {
+      setSelectedItem(setItem);
+      setSuccess(false);
+      setProgress(false);
+      setError(false);
+      setOpen(true);
+    },
+    [setSuccess, setProgress, setError, setOpen]
+  );
 
-	return {
-		isOpen,
-		showSuccess,
-		showProgress,
-		showError,
-		setSuccess,
-		setProgress,
-		setError,
-		closeModal,
-		openModal,
-		// @ts-ignore
-		item
-	};
+  return {
+    isOpen,
+    showSuccess,
+    showProgress,
+    showError,
+    setSuccess,
+    setProgress,
+    setError,
+    closeModal,
+    openModal,
+    // @ts-ignore
+    item,
+  };
 };
 
 interface IReturn<T> {
-	isOpen: boolean;
-	showSuccess: boolean;
-	showProgress: boolean;
-	showError: boolean;
-	setSuccess: React.Dispatch<React.SetStateAction<boolean>>;
-	setProgress: React.Dispatch<React.SetStateAction<boolean>>;
-	setError: React.Dispatch<React.SetStateAction<boolean>>;
-	closeModal: () => void;
-	openModal: (setItem?: any) => void;
-	item: T;
+  isOpen: boolean;
+  showSuccess: boolean;
+  showProgress: boolean;
+  showError: boolean;
+  setSuccess: React.Dispatch<React.SetStateAction<boolean>>;
+  setProgress: React.Dispatch<React.SetStateAction<boolean>>;
+  setError: React.Dispatch<React.SetStateAction<boolean>>;
+  closeModal: () => void;
+  openModal: (setItem?: any) => void;
+  item: T;
 }
 
 export default useModal;
