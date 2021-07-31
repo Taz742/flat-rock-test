f1f1f178af3e0e2b4549542ac98d3894cd78eb47
Tazo Leladze <t.leladze@omedia.ge>
2021-07-31 15:27:03 +0400

    make custom input component for react hook

---
+++ src/components/form/form-input.tsx
@@ -0,0 +1,36 @@
+import React, { FC, memo } from "react";
+import { useFormContext, Controller, useFormState } from "react-hook-form";
+import TextField from "@material-ui/core/TextField";
+
+export interface IProps {
+  name: string;
+  validate?: any;
+  disabled?: boolean;
+  label?: string;
+}
+
+const FormInput: FC<IProps> = ({
+  name,
+  disabled,
+  validate,
+  label,
+  ...props
+}) => {
+  const { control } = useFormContext();
+  const { errors } = useFormState({ control });
+
+  const error = errors && errors[name];
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={validate}
+      render={({ field }) => (
+        <TextField fullWidth {...field} label={label} error={error} />
+      )}
+    />
+  );
+};
+
+export default memo(FormInput);
--- src/components/users/add-or-update-user/index.tsx
+++ src/components/users/add-or-update-user/index.tsx
@@ -8,12 +8,15 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import { IUser, Role } from "../../../contexts/users/interfaces";
 
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
 import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
 import { options } from "./utils";
 import { emailPattern } from "../../../utils/validation";
 import useUsers from "../../../hooks/useUsers";
+
+import FormInput from "../../form/form-input";
+import Stack from "@material-ui/core/Stack";
 
 interface IFormInput {
   name: string;
@@ -33,16 +36,13 @@ export default function AddOrUpdateUser(props: IProps) {
 
   const { handleAddOrUpdateUser } = useUsers();
 
-  const {
-    control,
-    formState: { errors },
-    setValue,
-    handleSubmit,
-  } = useForm<IFormInput>({
+  const methods = useForm<IFormInput>({
     defaultValues: user || {
       role: "ADMIN",
     },
   });
+
+  const { setValue, handleSubmit } = methods;
 
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
     handleAddOrUpdateUser({
@@ -62,75 +62,51 @@ export default function AddOrUpdateUser(props: IProps) {
         fullWidth
         maxWidth={"sm"}
       >
-        <DialogTitle id="add-or-update-user" sx={{ marginBottom: '10px !important' }}>
+        <DialogTitle
+          id="add-or-update-user"
+          sx={{ marginBottom: "10px !important" }}
+        >
           {user ? "Edit User" : "Add New User"}
         </DialogTitle>
         <DialogContent>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div style={{ marginBottom: 20 }}>
-              <Controller
-                name="name"
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <TextField
-                    fullWidth
-                    {...field}
-                    label="Name"
-                    error={errors.name}
-                  />
-                )}
-              />
-            </div>
-            <div style={{ marginBottom: 20 }}>
-              <Controller
-                name="surname"
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <TextField
-                    fullWidth
-                    {...field}
-                    label="Surname"
-                    error={errors.name}
-                  />
-                )}
-              />
-            </div>
-            <div style={{ marginBottom: 20 }}>
-              <Controller
-                name="email"
-                control={control}
-                rules={{ required: true, pattern: emailPattern }}
-                render={({ field }) => (
-                  <TextField
-                    fullWidth
-                    {...field}
-                    label="Email"
-                    error={errors.email}
-                  />
-                )}
-              />
-            </div>
-            <div>
-              <TextField
-                select
-                fullWidth
-                label="Choose Role"
-                id="role"
-                defaultValue="ADMIN"
-                onChange={(e: any) => {
-                  setValue("role", e.target.value, true);
-                }}
-              >
-                {options.map((option) => (
-                  <MenuItem key={option.label} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </TextField>
-            </div>
-          </form>
+          <FormProvider {...methods}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Stack spacing={2}>
+                <FormInput
+                  name="name"
+                  label="Name"
+                  validate={{ required: true }}
+                />
+                <FormInput
+                  name="surname"
+                  label="Surname"
+                  validate={{ required: true }}
+                />
+                <FormInput
+                  type="email"
+                  name="email"
+                  label="Email"
+                  validate={{ required: true, pattern: emailPattern }}
+                />
+                <TextField
+                  select
+                  fullWidth
+                  label="Choose Role"
+                  id="role"
+                  defaultValue="ADMIN"
+                  onChange={(e: any) => {
+                    setValue("role", e.target.value, true);
+                  }}
+                >
+                  {options.map((option) => (
+                    <MenuItem key={option.label} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Stack>
+            </form>
+          </FormProvider>
         </DialogContent>
         <DialogActions>
           <Button onClick={onClose}>Disagree</Button>
